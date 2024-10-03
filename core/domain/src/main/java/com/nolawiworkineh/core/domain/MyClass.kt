34ac26a8cd2239fd9ea22c9147cb09b930720a34
noliw <noliw2017@gmail.com>
2024-10-03 00:37:32 -0700

    Initial commit

---
+++ core/domain/src/main/java/com/nolawiworkineh/core/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.nolawiworkineh.core.domain
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tasky"
+include(":app")
+include(":core:data")
+include(":core:presentation:designsystem")
+include(":core:presentation:ui")
+include(":core:domain")
+include(":core:database")
+include(":core:network")
